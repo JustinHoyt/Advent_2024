242901e7a98f954d72daeb17d30154c7db4e2aac
justinhoyt <justinhoyt@gmail.com>
2024-12-07 00:43:35 +0000

    Add 6.2 solution

--- day_6_guard_gallivant/guard_gallivant.ts
+++ day_6_guard_gallivant/guard_gallivant.ts
@@ -14,6 +14,7 @@ if (import.meta.main) {
     .map((line) => Array.from(line));
 
   console.log(countCellsVisited(grid));
+  console.log(countLoops(grid));
 }
 
 // Part 1
@@ -92,4 +93,151 @@ Deno.test("Part 1: Given example", () => {
     [".", ".", ".", ".", ".", ".", "#", ".", ".", "."],
   ];
   assertEquals(countCellsVisited(input), 41);
+});
+
+// Part 2
+
+function countLoops(grid: string[][]): number {
+  const obsructionsVisited = new Set<string>();
+
+  function isLoop(r: number, c: number, currDir: Dir): boolean {
+    const blockedTwice = (cell: string, coord: string) =>
+      cell === "#" && obsructionsVisited.has(coord);
+    const blocked = (cell: string) => cell === "#";
+    const exitedGrid = (cell: string) => cell === undefined;
+    const moveUp = () => r--;
+    const moveRight = () => c++;
+    const moveDown = () => r++;
+    const moveLeft = () => c--;
+
+    while (true) {
+      const coord = `${r},${c}: ${currDir}`;
+
+      switch (currDir) {
+        case Dir.UP: {
+          const nextCell: string | undefined = grid[r - 1]?.[c];
+
+          if (exitedGrid(nextCell)) return false;
+          if (blocked(nextCell) && obsructionsVisited.has(coord)) return true;
+
+          if (blocked(nextCell)) {
+            obsructionsVisited.add(coord);
+            currDir = Dir.RIGHT;
+          } else { // no blockages
+            moveUp();
+          }
+
+          break;
+        }
+        case Dir.RIGHT: {
+          const nextCell: string | undefined = grid[r]?.[c + 1];
+
+          if (exitedGrid(nextCell)) return false;
+          if (blocked(nextCell) && obsructionsVisited.has(coord)) return true;
+
+          if (blocked(nextCell)) {
+            obsructionsVisited.add(coord);
+            currDir = Dir.DOWN;
+          } else { // no blockages
+            moveRight();
+          }
+
+          break;
+        }
+        case Dir.DOWN: {
+          const nextCell: string | undefined = grid[r + 1]?.[c];
+
+          if (exitedGrid(nextCell)) return false;
+          if (blocked(nextCell) && obsructionsVisited.has(coord)) return true;
+
+          if (blocked(nextCell)) {
+            obsructionsVisited.add(coord);
+            currDir = Dir.LEFT;
+          } else { // no blockages
+            moveDown();
+          }
+
+          break;
+        }
+        case Dir.LEFT: {
+          const nextCell: string | undefined = grid[r]?.[c - 1];
+
+          if (exitedGrid(nextCell)) return false;
+          if (blocked(nextCell) && obsructionsVisited.has(coord)) return true;
+
+          if (blocked(nextCell)) {
+            obsructionsVisited.add(coord);
+            currDir = Dir.UP;
+          } else { // no blockages
+            moveLeft();
+          }
+
+          break;
+        }
+      }
+    }
+  }
+
+  const [startingRow, startingCol, currDir] = findGuardStartingPoint(grid);
+  let numLoops = 0;
+
+  for (let i = 0; i < grid.length; i++) {
+    for (let j = 0; j < grid[i].length; j++) {
+      // can't obstruct the guard's starting point
+      if (i === startingRow && j === startingCol) continue;
+      // can't block if it's already obstructed
+      if (grid[i][j] === "#") continue;
+
+      grid[i][j] = "#";
+      numLoops += Number(isLoop(startingRow, startingCol, currDir));
+      obsructionsVisited.clear();
+      grid[i][j] = ".";
+    }
+  }
+  return numLoops;
+}
+
+// The O's in the test are purely for illustrative purposes.
+// They denote where the added obstructions should be placed
+Deno.test("Part 2: Given example", () => {
+  const input = [
+    [".", ".", ".", ".", "#", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", ".", "#"],
+    [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", "#", ".", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", "#", ".", "."],
+    [".", ".", ".", ".", ".", ".", ".", ".", ".", "."],
+    [".", "#", ".", "O", "^", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", "O", "O", "#", "."],
+    ["#", "O", ".", "O", ".", ".", ".", ".", ".", "."],
+    [".", ".", ".", ".", ".", ".", "#", "O", ".", "."],
+  ];
+  assertEquals(countLoops(input), 6);
+});
+
+Deno.test("Part 2: zero loops", () => {
+  const input = [
+    [".", ".", ".", ".", "#"],
+    [".", ".", ".", ".", "."],
+    [".", ".", ".", ".", "."],
+    [".", ".", "#", ".", "."],
+    [".", ".", ".", ".", "."],
+    [".", ".", ".", ".", "."],
+    [".", "#", ".", ".", "^"],
+    [".", ".", ".", ".", "."],
+    ["#", ".", ".", ".", "."],
+    [".", ".", ".", ".", "."],
+  ];
+  assertEquals(countLoops(input), 0);
+});
+
+Deno.test("Part 2: loop that goes only up and down", () => {
+  const input = [
+    ["#", "#", "."],
+    [".", ".", "O"],
+    ["^", ".", "."],
+    ["#", ".", "."],
+    [".", "#", "#"],
+  ];
+  assertEquals(countLoops(input), 1);
 });
